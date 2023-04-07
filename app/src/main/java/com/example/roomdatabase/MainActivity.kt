617c8562be00bb1191f9fef85d953d83be01d0c0
Neha Dhoble <neha@gmail.com>
2023-04-07 18:19:31 +0530

    updated dependencies version

--- app/src/main/java/com/example/roomdatabase/MainActivity.kt
+++ app/src/main/java/com/example/roomdatabase/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         appDb = AppDatabase.getDatabase(this)
         binding.btnWriteData.setOnClickListener {
             writeData()
